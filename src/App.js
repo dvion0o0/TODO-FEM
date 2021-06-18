343da9f5dbf091aa29c46c69abcc3c65515f179f
Dv <dv@Dvs-MacBook-Pro.local>
2021-06-19 00:55:22 +0530

    Todo App Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
+import TodoForm from "./components/TodoForm";
+import Footer from "./components/Footer";
+import TodoList from "./components/TodoList";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem("list")));
+  } else {
+    return data;
+  }
+};
+
+const data = [
+  { id: 1, content: "Complete online JavsScript course", completed: true },
+  { id: 2, content: "Learn Javascript Framework", completed: false },
+  { id: 3, content: "Learn CSS Framework", completed: false },
+  { id: 4, content: "Read for 1 hour", completed: false },
+  { id: 5, content: "Take a break", completed: false },
+  { id: 6, content: "Complete Todo App on Frontend Mentor", completed: false },
+];
 
 function App() {
+  const [todos, setTodos] = useState(getLocalStorage());
+  const [themeLight, setThemeLight] = useState(true);
+  const [filterStatus, setFilterStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState(todos);
+
+  const themeClass = themeLight ? "light" : "dark";
+
+  useEffect(() => {
+    const handleFilter = () => {
+      switch (filterStatus) {
+        case "active":
+          return setFilteredTodos(todos.filter((todo) => !todo.completed));
+
+        case "completed":
+          return setFilteredTodos(todos.filter((todo) => todo.completed));
+
+        default:
+          return setFilteredTodos(todos);
+      }
+    };
+    handleFilter();
+  }, [todos, filterStatus]);
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(todos));
+  }, [todos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`wrapper ${themeClass}`}>
+      <div className="container">
+        <Header themeLight={themeLight} setThemeLight={setThemeLight} />
+        <main>
+          <TodoForm todos={todos} setTodos={setTodos} />
+          <TodoList
+            todos={todos}
+            setTodos={setTodos}
+            filteredTodos={filteredTodos}
+            filterStatus={filterStatus}
+            setFilterStatus={setFilterStatus}
+            setFilteredTodos={setFilteredTodos}
+          />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
